f4dff1c8558a494f75a88ff8b605b9be24019ba9
27597581 <27597581@qq.com>
2018-04-12 18:30:40 +0800

    第一次提交

---
+++ src/main/java/cn/edu/hqu/javaee/CDPlayerConfig.java
@@ -0,0 +1,18 @@
+package cn.edu.hqu.javaee;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import cn.edu.hqu.javaee.domain.cd.CompactDisc;
+import cn.edu.hqu.javaee.domain.cd.impl.CDPlayer;
+
+@Configuration
+@Import(CDConfig.class)
+public class CDPlayerConfig {
+	@Bean
+	public CDPlayer cdPlayer(CompactDisc compactDisc) {
+		//直接注入
+		return new CDPlayer(compactDisc);
+	}
+}
